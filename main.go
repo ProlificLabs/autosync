ec32ca09cc8d5b4915925fd797b854b068e29331
Daniel Dangond <daniel.dangond@pulley.com>
2025-05-06 09:22:18 -0400

    Updated main.go

--- main.go
+++ main.go
@@ -45,6 +45,12 @@ func main() {
 	}
 	fmt.Printf("Modified YDoc JSON after UpdateToState: %+v\n", modifiedState)
 
+	firstStateVector, err := doc.GetStateVector()
+	if err != nil {
+		fmt.Println("Error getting first state vector:", err)
+		return
+	}
+
 	emptyState := make(map[string]interface{})
 	patch, err = autosyncdoc.UpdateToState(doc, emptyState)
 	if err != nil {
@@ -53,6 +59,12 @@ func main() {
 	}
 	fmt.Printf("Generated Patch: %+v\n", patch.List())
 
+	secondStateVector, err := doc.GetStateVector()
+	if err != nil {
+		fmt.Println("Error getting second state vector:", err)
+		return
+	}
+
 	// Get and print the final state
 	finalState, err := doc.ToJSON()
 	if err != nil {
@@ -60,4 +72,59 @@ func main() {
 		return
 	}
 	fmt.Printf("Final YDoc JSON after UpdateToState: %+v\n", finalState)
+
+	err = doc.ApplyStateVector(firstStateVector)
+	if err != nil {
+		fmt.Println("Error applying first state vector:", err)
+		return
+	}
+
+	firstSVState, err := doc.ToJSON()
+	if err != nil {
+		fmt.Println("Error getting first state vector JSON:", err)
+		return
+	}
+	fmt.Printf("First State Vector JSON: %+v\n", firstSVState)
+
+	err = doc.ApplyStateVector(secondStateVector)
+	if err != nil {
+		fmt.Println("Error applying second state vector:", err)
+		return
+	}
+
+	secondSVState, err := doc.ToJSON()
+	if err != nil {
+		fmt.Println("Error getting second state vector JSON:", err)
+		return
+	}
+	fmt.Printf("Second State Vector JSON: %+v\n", secondSVState)
+
+	newDoc := autosyncdoc.NewAutoSyncDoc()
+	defer newDoc.Destroy()
+
+	err = newDoc.ApplyStateVector(firstStateVector)
+	if err != nil {
+		fmt.Println("Error applying first state vector to new doc:", err)
+		return
+	}
+
+	newDocSVState, err := newDoc.ToJSON()
+	if err != nil {
+		fmt.Println("Error getting new doc state vector JSON:", err)
+		return
+	}
+	fmt.Printf("New Doc State Vector JSON: %+v\n", newDocSVState)
+
+	err = newDoc.ApplyStateVector(secondStateVector)
+	if err != nil {
+		fmt.Println("Error applying second state vector to new doc:", err)
+		return
+	}
+
+	newDocSecondSVState, err := newDoc.ToJSON()
+	if err != nil {
+		fmt.Println("Error getting new doc second state vector JSON:", err)
+		return
+	}
+	fmt.Printf("New Doc Second State Vector JSON: %+v\n", newDocSecondSVState)
 }
